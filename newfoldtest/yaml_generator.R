7cf6f411cbfdb1dae2d85e34512bbf1f3037b473
tori-cowger <63074745+tori-cowger@users.noreply.github.com>
2023-01-26 00:23:56 -0500

    updating page docs

---
+++ newfoldtest/yaml_generator.R
@@ -0,0 +1,20 @@
+library("ymlthis")
+
+yml_empty() %>%
+  yml_site_opts(
+    name = "my-website",
+    output_dir =  "_site",
+    exclude = c("docs.txt", "*.csv", "newfoldtest"),
+    include = "_navbar.html"
+  ) %>%
+  yml_navbar(
+    title = "NULL",
+    left = list(
+      navbar_page("Home", href = "index.html", favicon = "fa-home"),
+      navbar_page(navbar_separator(), href = "about.html")
+    )
+  ) %>%
+  yml_output(html_document(toc = TRUE, highlight = "textmate"))
+
+
+navbar_separator()
